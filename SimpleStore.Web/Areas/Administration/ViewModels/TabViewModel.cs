241871fef91af9bd85f34efe1b83793f98894eb8
qualterz <qualterz@tutamail.com>
2021-09-04 18:47:32 +0300

    Add tabs

---
+++ SimpleStore.Web/Areas/Administration/ViewModels/TabViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleStore.Web.Areas.Administration.ViewModels
+{
+    public enum TabType
+    {
+        Items,
+    }
+}
